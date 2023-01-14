a79d733a99b6bc65b049aaa2e3f381de9e36a786
mdArifulIslam12 <mdarif767778@gmail.com>
2023-01-14 09:07:40 +0600

    least

--- src/App.js
+++ src/App.js
@@ -9,8 +9,19 @@ import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import Footer from './component/Shared/Footer/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [displayNone,setDisplayNone] = useState(false)
+
+  const allScroolId = () =>{
+    if(window.scrollY >= 350){
+      setDisplayNone(true)
+    }else{
+      setDisplayNone(false)
+    }
+  }
+  window.addEventListener('scroll',allScroolId)
   return (
     <div className="app" id='scrool'>
       <Navbar/>
@@ -20,7 +31,7 @@ function App() {
         <Route path='login' element={<Login/>} />
       </Routes>
       <ToastContainer/>
-      <div className='all-scrool'>
+      <div className={displayNone ? 'all-scrool displayBlock' : 'all-scrool displayNone'}>
       <a href="#scrool"><FontAwesomeIcon className='all-scrool-icon' icon={faArrowUp}/></a>
       </div>   
      
